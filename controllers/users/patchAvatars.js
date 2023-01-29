808ecb210ea1d80ec44ccc28b814e21b7b098059
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-01-29 13:16:00 +0200

    upDate patchAvatar

--- controllers/users/patchAvatars.js
+++ controllers/users/patchAvatars.js
@@ -6,10 +6,12 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const patchAvatars = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
+  const { _id: id } = req.user;
+  const imageName = `${id}_${originalname}`;
   try {
-    const resultUpload = path.join(avatarsDir, originalname);
+    const resultUpload = path.join(avatarsDir, imageName);
     fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("public", "avatars", originalname);
+    const avatarURL = path.join("public", "avatars", imageName);
     console.log(avatarURL);
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
     res.json({ avatarURL });
